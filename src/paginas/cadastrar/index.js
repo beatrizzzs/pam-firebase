92cd6916795dc784ce1c85ccb5b914f6f1ab8dca
beatrizzzs <beatrizslima123@gmail.com>
2022-10-18 10:46:47 -0300

    Adiconando cadastro

--- src/paginas/cadastrar/index.js
+++ src/paginas/cadastrar/index.js
@@ -1,22 +1,95 @@
-import React from 'react'
 
-import {View,Text,} from 'react-native'
-
-
-
+import {View,Text,TextInput,StyleSheet,TouchableOpacity} from 'react-native'
+import firebase from '../../firebaseConnection'
+import React, {useState} from 'react'
 
 export default function Cadastrar(){
+     const [nome, setNome] = useState("");
+     const [nota1, setNota1] = useState("");
+     const [nota2, setNota2] = useState("");
+     const [nota3, setNota3] = useState("");
+     const [imagem, setImagem] = useState("");
 
-return(
+     async function cadastrar(){
 
-    <View>
+        const alunos = await firebase.database ().ref('Alunos');
+        const chave = alunos.push().key;
 
-    <Text> CADASTRAR</Text>
+        alunos.child(chave).set ({
 
+            Nome: nome,
+            Nota1: nota1,
+            Nota2: nota2,
+            Nota3: nota3,
+            Imagem: imagem
+
+        })
+
+         }
+
+    return(
+
+
+
+        <View style ={estilos.container}>
+        <Text style={{fontSize:30, fontWeight:'bold'}}>Cadastro de Alunos </Text>
+        <Text style ={estilos.texto}>Nome :</Text>
+        <TextInput  style ={estilos.entradas} placeholder='Digite o nome do aluno' 
+        onChangeText = {(texto) => setNome (texto) } 
+
+        ></TextInput>
+
+    
+        <Text style ={estilos.texto}>Nota1 :</Text>
+        <TextInput  style ={estilos.entradas} onChangeText = {(texto) => setNota1 (texto) } placeholder='Digite a Nota 1 do aluno'></TextInput>
+        <Text style ={estilos.texto}>Nota2 :</Text>
+        <TextInput  style ={estilos.entradas} onChangeText = {(texto) => setNota2 (texto) } placeholder='Digite a Nota 2 do aluno'></TextInput>
+        <Text style ={estilos.texto}>Nota3 :</Text>
+        <TextInput  style ={estilos.entradas} onChangeText = {(texto) => setNota3 (texto) } placeholder='Digite a Nota 3 do aluno'></TextInput>
+        <Text style ={estilos.texto}>Imagem :</Text>
+        <TextInput style ={estilos.entradas} onChangeText = {(texto) => setImagem (texto) } placeholder='Digite o link com a foto do aluno' ></TextInput>
+
+
+    <TouchableOpacity style ={estilos.botao} onPress={cadastrar}>
+        <Text style={{fontWeight:'bold  '}}>Cadastrar</Text></TouchableOpacity>
     </View>
 
 
 );
 
-
 }
+
+const estilos = StyleSheet.create({
+
+    container:{
+        flex:1,
+        alignItems:'center'
+
+    },
+    entradas :{
+        width:'85%',
+        height:30,
+        borderWidth:2
+    },
+    botao:{
+        width:'85%',
+        height:60,
+        alignItems:'center',
+        backgroundColor:'orange',
+        marginTop:20,
+        justifyContent:'center',
+        fontWeight:'bold'
+    },
+    texto:{
+        marginTop:20,
+        marginLeft:-252,
+        fontSize:15,
+        fontWeight:'bold'
+
+
+    }
+
+
+})
+
+
